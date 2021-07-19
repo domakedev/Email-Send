d3a0382e8c78f06c99994ae3b3ca845277026666
Cesar Guevara Cabrera <sarcansino@gmail.com>
2021-07-18 23:48:22 -0500

    Diseño y maquetacion lista!

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    'index.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      translate: ['active'],
+      backgroundColor: ['active']
+    },
+  },
+  plugins: [],
+}
